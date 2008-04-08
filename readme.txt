c604d42dd8dbd007ec6775c2300eb160fef050ac
dirigenteitis <dirigenteitis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-04-08 09:16:03 +0000

    tagging version 1.3
    git-svn-id: https://plugins.svn.wordpress.org/wdm-news/tags/1.3@39154 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,16 +1,16 @@
 === WDM News ===
 Contributors: Walter Dal Mut
 Donate link: http://www.manosdepiedra.com/
-Tags: news, little post
+Tags: news, little post, sentence
 Requires at least: 1.0
-Tested up to: 1.0
-Stable tag: 1.0
+Tested up to: 2.2
+Stable tag: 1.3
 
 This plugin show like widget a little sentence with date and time of publication.
 
 == Description ==
 
-This plugin show like widget a little sentence with date and time of publication. It's a news system for explain short message on sidebar.
+This plugin show like widget a little sentence with date and time of publication. It's a news system for explain short message on sidebar. You can link your news at existing page or web site. 
 
 == Installation ==
 
@@ -24,14 +24,24 @@ e.g.
 4. Go to Widget section and adding 'WDM News' in sidebar
 5. Go to Write section and select 'WDM News Adding'. Write a sentence in text field and add it! You can see appear under with checkbox if you want delete it.
 6. End, use it!
+Bye
+Walter
 
 == Frequently Asked Questions ==
 
 = Why not adding a news at first time? =
 
-It's possibile if script can't create table in your MySQL account. You can download wdmnews_db_mysql.txt and import in your wordpress database, now run correctly. You can find this file on http://www.manosdepiedra.com/wdm-plugins/wdmnews_db_mysql.sql
+It's possibile if script can't create table in your MySQL account. You can download wdmnews_db_mysql.txt and import in your wordpress database, now run correctly. You can find this file on http://www.manosdepiedra.com/wp-content/uploads/2008/04/wdmnews-db-mysql.sql
 
 == Screenshots ==
 
 1. Menu for insert new News.
 2. In main page a list of News.
+3. Uninstall page for eliminate all contributes of WDM News
+
+== Uninstall ==
+
+If you want unistall WDM News you go to plugins section and click on 'WDM News Uninstall', before this step you have completely remove plugins, you olny deactivate plugin from plugins section.
+
+== Upgrading ==
+If you have already install a previous version for upgrade this system you have only overwrite wdm-news file in wdm-news plugin directory.
--- wdm-news.php
+++ wdm-news.php
@@ -3,11 +3,10 @@
 Plugin Name: WDM News
 Plugin URI: http://www.manosdepiedra.com
 Description: WDM News show your news on sidebar. When you activate it, you can move widget on left or right sidebar for show it. For adding or remove news you can use a submenu on plugin.
-Version: 1.0
+Version: 1.3
 Author: Walter Dal Mut
 Author URI: walter@manosdepiedra.com
 */
-
 add_action('init', 'wdmnews_init');
 
 function widget_wdmnews_init() {
@@ -21,11 +20,11 @@ function widget_wdmnews_init() {
 		global $table_prefix, $wpdb, $user_level;
 		echo '<h2 class="widgettitle">News</h2>';
 		echo '<ul>';
-			$query = "SELECT news, data FROM " . $table_prefix . "wdmnews ORDER BY data desc";
+			$query = "SELECT news, link, data FROM " . $table_prefix . "wdmnews ORDER BY data desc";
 			
 			$news = $wpdb->get_results($query);
 			foreach ($news as $new) {
-				echo '<li><b>'.$new->data.'</b><br />'.$new->news."</li>";
+				echo '<li><a href="'.$new->link.'"><b>'.$new->data.'</b><br />'.$new->news."</a></li>";
 			}
 		echo '</ul>';
 	}
@@ -35,9 +34,7 @@ add_action('plugins_loaded', 'widget_wdmnews_init');
 
 
 function wdmnews_init()
-{
-	wdmnews_install();	 
-	
+{	
  	add_action('admin_menu', 'wdmnews_config_page');
 }
 
@@ -47,11 +44,11 @@ function wdmnews_install()
 	
 	$table_name = $table_prefix . "wdmnews";
 	
-	$sql = "CREATE TABLE ".$table_name." (
+	$sql = "CREATE TABLE IF NOT EXISTS ".$table_name." (
  	     news_id mediumint(9) NOT NULL AUTO_INCREMENT,
  	     news text NOT NULL,
  	     data datetime NOT NULL,
- 	     PRIMARY KEY (news_id)
+ 	     UNIQUE KEY news_id (news_id)
  	   );";
 	
 	get_currentuserinfo();
@@ -59,12 +56,49 @@ function wdmnews_install()
     
     require_once(ABSPATH . 'wp-admin/upgrade-functions.php');
     dbDelta($sql);  
+    
+    $sql = "ALTER TABLE ".$table_name." ADD link VARCHAR( 255 ) NOT NULL AFTER news";
+    $wpdb->query($sql);
 }
 
 function wdmnews_config_page() {
 	if ( function_exists('add_submenu_page') )
+	{
 		add_submenu_page('post.php', __('WDM News Adding'), __('WDM News Adding'), 'manage_options', 'wdmnews-key-config', 'wdmnews_conf');
+		add_submenu_page('plugins.php', __('WDM News Uninstall'), __('WDM News Uninstall'), 'manage_options', 'wdmnews-key-uninstall', 'wdmnews_uninstall'); 
+	}
 	
+}
+
+function wdmnews_uninstall()
+{
+	global $table_prefix, $wpdb, $user_level;
+	
+	$table_name = $table_prefix . "wdmnews";
+	?>
+	<div class="wrap">
+	<h2><?php _e('WDM News Uninstall'); ?></h2>
+	<p>If you want completely remove this pluging, you push this link:</p>
+	<div class="narrow">
+		<p><a href="?page=wdmnews-key-uninstall&delete=true">Uninstall</a></p>
+		<?php
+			if( $_GET["delete"] )
+			{
+			 	$sql = "DROP TABLE IF EXISTS ".$table_name;
+			 	get_currentuserinfo();
+	    		if ($user_level < 8) { return; }
+				
+				$wpdb->query($sql);
+				
+				echo '<div id="message" class="updated fade">Uninstall successful.</div>';
+				echo '<p>You must deactivate plugin from plugins page for complete uninstall.</p>';
+			}
+			
+		
+		?>
+	</div>
+	</div>
+	<?php 	
 }
 
 function wdmnews_conf() 
@@ -86,27 +120,28 @@ function wdmnews_conf()
 			{
 				$ok = $wpdb->query('DELETE FROM '.$table_prefix."wdmnews".' WHERE news_id = '.$news_id);
 				if( $ok == FALSE )
-					echo '<div id="message" class="updated fade"><p>Impossibile delete news</p></div>';
+					echo '<div id="error" class="updated fade"><p>Impossibile to delete news</p></div>';
 				else
 					echo '<div id="message" class="error"><p>News deleted</p></div>';
 			}
 	}
 ?>	
 	<div class="wrap">
-	<h2><?php _e('WDM News Adding'); ?></h2>
+	<h2><?php _e('WDM News Adding'); wdmnews_install();?></h2>
 	<div class="narrow">
 		<form method="POST" action="">
-			<p><b>Insert a news:</b><input type="text" name="news" value="" size="50"/></p>
+			<p><b>Insert a news:</b><input type="text" name="news" value="" size="50" /></p>
+			<p><b>Link this news:<br />http://</b></span><input type="text" name="link" value="" size="40" /></p>
 			<p align="right"><input type="submit" name="submit" value="Send News" /></p>		
 		</form>
 		<form method="POST" action="">
 		<?php
 			//Show news...
-			$query = "SELECT news_id, news, data FROM " . $table_prefix . "wdmnews";
+			$query = "SELECT news_id, news, link, data FROM " . $table_prefix . "wdmnews ORDER BY data desc";
 			
 			$news = $wpdb->get_results($query);
 			foreach ($news as $new) {
-				echo '<p><input type="checkbox" name="news[]" value="'.$new->news_id.'" />'.$new->news."</p>";
+				echo '<p><input type="checkbox" name="news[]" value="'.$new->news_id.'" />'.$new->news." (".$new->link.")</p>";
 			}
 			
 			echo '<p align="right"><input type="submit" name="delnews" value="Delete checked" /></p>';
@@ -122,7 +157,8 @@ function wdmnews_add()
 	global $table_prefix, $wpdb, $user_level;
 	
 	$news = htmlentities($_POST["news"], ENT_QUOTES, "UTF-8");
- 	$query = "INSERT INTO ".$table_prefix . "wdmnews"." ( news, data ) VALUES ( '$news', NOW() )";
+	$link = htmlentities($_POST["link"], ENT_QUOTES, "UTF-8");
+ 	$query = "INSERT INTO ".$table_prefix . "wdmnews"." ( news, link, data ) VALUES ( '$news', 'http://$link', NOW() )";
  	$wpdb->query($query);
 }
 ?>
