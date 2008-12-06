2284806da2c188ee2d583af832aafc0ce3349d78
dirigenteitis <dirigenteitis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-06 12:03:50 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wdm-news/trunk@77861 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wdm-news.php
+++ wdm-news.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: WDM News
-Plugin URI: http://www.manosdepiedra.com
+Plugin URI: http://www.walterdalmut.com
 Description: WDM News show your news on sidebar. When you activate it, you can move widget on left or right sidebar for show it. For adding or remove news you can use a submenu on plugin.
-Version: 1.8
+Version: 1.9
 Author: Walter Dal Mut
-Author URI: walter@manosdepiedra.com
+Author URI: info@walterdalmut.com
 */
 add_action('init', 'wdmnews_init');
 register_activation_hook( __FILE__, 'wdmnews_install' );
